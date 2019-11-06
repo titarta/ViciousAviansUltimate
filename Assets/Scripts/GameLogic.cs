f2636ce890e955556c696ccd205352f3cf358496
titarta <tiagorvcarvalho@gmail.com>
2019-11-06 12:52:26 +0000

    things are taking shape

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    private bool wasFired;
+    private Rigidbody birdRB;
     public Transform birdLaunchPlaceHolder;
     public GameObject bird;
     public GameObject slingshotMarker;
@@ -11,7 +13,8 @@ public class GameLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        birdRB = bird.GetComponent<Rigidbody>();
+        wasFired = false;
     }
 
 
@@ -21,42 +24,45 @@ public class GameLogic : MonoBehaviour
         //Update bird Position
 
         // Get bird XY position
-        Vector3 slingshotBackwardDirection = slingshotMarker.transform.forward * -1;
-        Vector3 markerToMarkerVector = birdMarker.transform.position - slingshotMarker.transform.position;
-        
-        Vector2 slingshotBackwardDirection2D = new Vector2(slingshotBackwardDirection.z, -slingshotBackwardDirection.x);
-        slingshotBackwardDirection2D = slingshotBackwardDirection2D.normalized;
+        if(!wasFired) {
+            Vector3 slingshotBackwardDirection = slingshotMarker.transform.forward * -1;
+            Vector3 markerToMarkerVector = birdMarker.transform.position - slingshotMarker.transform.position;
+            
+            Vector2 slingshotBackwardDirection2D = new Vector2(slingshotBackwardDirection.z, -slingshotBackwardDirection.x);
+            slingshotBackwardDirection2D = slingshotBackwardDirection2D.normalized;
 
-        Vector2 markerToMarkerVector2D = new Vector2(markerToMarkerVector.x, markerToMarkerVector.z);
+            Vector2 markerToMarkerVector2D = new Vector2(markerToMarkerVector.x, markerToMarkerVector.z);
 
-        float distanceBirdToSlingshot = Vector2.Dot(slingshotBackwardDirection2D, markerToMarkerVector2D);
+            float distanceBirdToSlingshot = Vector2.Dot(slingshotBackwardDirection2D, markerToMarkerVector2D);
 
-        Vector2 slingshotMarkerPos2D = new Vector2(slingshotMarker.transform.position.x, slingshotMarker.transform.position.z);
-        Vector2 birdMarkerPos2D = new Vector2(birdMarker.transform.position.x, birdMarker.transform.position.z);
-        Vector2 birdXYpos = slingshotMarkerPos2D + slingshotBackwardDirection2D*distanceBirdToSlingshot;
+            Vector2 slingshotMarkerPos2D = new Vector2(slingshotMarker.transform.position.x, slingshotMarker.transform.position.z);
+            Vector2 birdMarkerPos2D = new Vector2(birdMarker.transform.position.x, birdMarker.transform.position.z);
+            Vector2 birdXYpos = slingshotMarkerPos2D + slingshotBackwardDirection2D*distanceBirdToSlingshot;
 
-        // Get bird Y position
-        float increasedAltitude = 0;
-        if(isLeft(slingshotMarkerPos2D,birdMarkerPos2D,birdXYpos )) {
-            increasedAltitude = Vector2.Distance(birdXYpos,  birdMarkerPos2D);
-        } else {
-            increasedAltitude = - Vector2.Distance(birdXYpos,  birdMarkerPos2D);
+            // Get bird Y position
+            float increasedAltitude = 0;
+            if(isLeft(slingshotMarkerPos2D,birdMarkerPos2D,birdXYpos )) {
+                increasedAltitude = Vector2.Distance(birdXYpos,  birdMarkerPos2D);
+            } else {
+                increasedAltitude = - Vector2.Distance(birdXYpos,  birdMarkerPos2D);
+            }
+            
+            Vector3 crossProductAux = Vector3.Cross(slingshotBackwardDirection, markerToMarkerVector);
+
+            if(crossProductAux.y > 0) {
+                bird.transform.position = new Vector3(birdXYpos.x,  birdLaunchPlaceHolder.transform.position.y + increasedAltitude , birdXYpos.y);
+            } else {
+                bird.transform.position = new Vector3(birdXYpos.x, birdLaunchPlaceHolder.transform.position.y + increasedAltitude , birdXYpos.y);
+            }
+
+            //Check user input
+            if(Input.touchCount >= 1) {
+                shootBird();
+            }
+
         }
-        
-        Vector3 crossProductAux = Vector3.Cross(slingshotBackwardDirection, markerToMarkerVector);
-
-        if(crossProductAux.y > 0) {
-            bird.transform.position = new Vector3(birdXYpos.x,  birdLaunchPlaceHolder.transform.position.y + increasedAltitude , birdXYpos.y);
-        } else {
-            bird.transform.position = new Vector3(birdXYpos.x, birdLaunchPlaceHolder.transform.position.y + increasedAltitude , birdXYpos.y);
-        }
-
-        //Debug.Log(bird.transform.position);
-
-        //Check user input
 
         
-
        
 
     }
@@ -65,10 +71,8 @@ public class GameLogic : MonoBehaviour
         return ((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x)) > 0;
     }
 
-    void OnGUI() {
-        bool fireButton = GUI.Button(new Rect(10, 10, 100, 30), "Make it fly!");
-        if(fireButton) {
-            Debug.Log("worked");
-        }
+    private void shootBird() {
+        birdRB.AddForce(10, 0, 0, ForceMode.VelocityChange);
+        wasFired = true;
     }
 }
