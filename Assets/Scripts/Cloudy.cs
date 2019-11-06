f0cee852bf00f25e87756b7303d9b65d7b280172
unknown <up201505318@fe.up.pt>
2019-11-06 17:48:40 +0000

    added weathers

---
+++ Assets/Scripts/Cloudy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloudy : MonoBehaviour
+{
+    public GameObject lightObj;
+    private Light light;
+    void Start()
+    {
+        light = lightObj.GetComponent<Light>();
+    }
+
+    void Update () 
+	{
+		if(light.intensity > 4)
+        {
+            light.intensity = light.intensity - 4;
+        } else {
+            light.intensity = 1;
+        }
+	}
+}
---
+++ Assets/Scripts/NightTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightTime : MonoBehaviour
+{
+    public GameObject lightObj;
+    private Light light;
+    void Start()
+    {
+        light = lightObj.GetComponent<Light>();
+    }
+
+    void Update () 
+	{
+		light.intensity = 0;
+	}
+}
