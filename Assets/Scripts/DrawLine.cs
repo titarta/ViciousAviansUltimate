092c9bcd20c1f78cd711cf29536aebb7786dc003
titarta <tiagorvcarvalho@gmail.com>
2019-11-06 10:02:05 +0000

    Drawline

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform startPoint;
+    public Transform endPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetWidth(.45f, .45f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, startPoint.position);
+        lineRenderer.SetPosition(1, endPoint.position);
+    }
+}
